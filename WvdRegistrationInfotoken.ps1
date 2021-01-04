aa437f90a3df9c69a44b5caa6d0fde730a62b080
Freek Berson <freek.berson@wortell.nl>
2021-01-04 09:52:41 +0100

    Update WvdRegistrationInfotoken.ps1

--- WvdRegistrationInfotoken.ps1
+++ WvdRegistrationInfotoken.ps1
@@ -1,13 +1,15 @@
+param (
+    [string]$azureSubscriptionID,
+    [string]$resourceGroupName,
+    [string]$existingWVDHostPoolName
+)
+
 import-module az.desktopvirtualization
 import-module az.network
 import-module az.compute
 
-$azureSubscriptionID = "66869840-a086-41d1-84e9-cf66ac8a9a94"
-$resourceGroupName = "BICEP-WVD-DEMO-A-BACKPLANE-RG"
-$existingWVDHostPoolName = "BICEP-A-WVD-HP"
-
 $Registered = Get-AzWvdRegistrationInfo -SubscriptionId "$azureSubscriptionID" -ResourceGroupName "$resourceGroupName" -HostPoolName $existingWVDHostPoolName
-if (-not(-Not $Registered.Token)){$registrationTokenValidFor = (NEW-TIMESPAN -Start (get-date) -End $Registered.ExpirationTime | select Days,Hours,Minutes,Seconds)}
+if (-not(-Not $Registered.Token)){$registrationTokenValidFor = (NEW-TIMESPAN -Start (get-date) -End $Registered.ExpirationTime | select-object Days,Hours,Minutes,Seconds)}
 
 $registrationTokenValidFor
 if ((-Not $Registered.Token) -or ($Registered.ExpirationTime -le (get-date)))

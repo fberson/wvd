4bbcc7bb1cf7011838290963e15d6d904937e2ca
Freek Berson <freek.berson@wortell.nl>
2021-07-09 14:16:39 +0200

    MSIX app attach scrtips

---
+++ MSIX app attach/DeStage.ps1
@@ -0,0 +1,14 @@
+﻿#MSIX app attach de staging sample
+
+#region variables
+$packageName = "Notepadplusplus_1.0.0.0_x64__vcbnmdqcr7aap"
+$msixJunction = "C:\temp\AppAttach\"
+$vhdSrc="\\share\AppAttach\NotepadPlusPlus.vhd"
+#endregion
+
+#region deregister
+Remove-AppxPackage -AllUsers -Package $packageName 
+cd $msixJunction
+Remove-Item $packageName -Force -Recurse
+dismount-vhd -Path $vhdSrc
+#endregion
---
+++ MSIX app attach/Register.ps1
@@ -0,0 +1,10 @@
+﻿#MSIX app attach registration sample
+
+#region variables
+$packageName = "Notepadplusplus_1.0.0.0_x64__vcbnmdqcr7aap"
+$path = "C:\Program Files\WindowsApps\" + $packageName + "\AppxManifest.xml"
+#endregion
+
+#region register
+Add-AppxPackage -Path $path -DisableDevelopmentMode -Register
+#endregion
